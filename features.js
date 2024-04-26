7cb643356d94b06fce03c9ab949f9560ca3d9381
AyushSaxena092000 <ayushsaxena092000@gmail.com>
2024-04-26 13:23:01 +0530

    nodejsscretch

---
+++ features.js
@@ -0,0 +1,5 @@
+export const generateLovePercent = () => {
+  return `${Math.floor(Math.random() * 100)}%`;
+};
+
+// new thing replacement of Math.floor we can use ~~ this also in place of Math.floor
